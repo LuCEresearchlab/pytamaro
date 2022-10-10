b3ccdc169088ab362ff0fccacab4205b078d6cd4
Luca Chiodini <luca@chiodini.org>
2022-10-10 17:40:37 +0200

    docs: sync version in the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021-2022, LuCE Research Lab"
 author = "Luca Chiodini"
 
 # The full version, including alpha/beta/rc tags
-release = "0.1"
+release = "0.2"
 
 
 # -- General configuration ---------------------------------------------------
