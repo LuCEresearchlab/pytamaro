4880c1ef58901c49a433edecb010723d459d9d9a
Arnaud Fauconnet <riukkarma@gmail.com>
2023-11-17 13:56:10 +0100

    docs: created tests for operations

--- tests/fr/test_operations.py
+++ tests/fr/test_operations.py
@@ -1,45 +1,53 @@
 from pytamaro.color_names import blue, red
-from pytamaro.it.operations import (accanto, altezza_grafica, componi, fissa,
-                                    larghezza_grafica, ruota, sopra,
-                                    sovrapponi)
+from pytamaro.fr.point_names import *
+# from pytamaro import operations as en
 from pytamaro.operations import (above, beside, compose, graphic_height,
                                  graphic_width, overlay, pin, rotate)
+from pytamaro.fr.operations import (largeur_graphique, hauteur_graphique, ancre,
+superpose, cote_a_cote, au_dessus, pivote)
+from pytamaro.fr.operations import compose as fr_compose
+
+
 from pytamaro.primitives import rectangle
-from tests.testing_utils import HEIGHT, WIDTH
-from pytamaro.it.point_names import *
+from tests.testing_utils import HEIGHT, WIDTH, assert_repr
 
 g = rectangle(WIDTH, HEIGHT, red)
 
 
 def test_width():
-    assert graphic_width(g) == larghezza_grafica(g)
+    assert graphic_width(g) == largeur_graphique(g)
 
 
 def test_height():
-    assert graphic_height(g) == altezza_grafica(g)
+    assert graphic_height(g) == hauteur_graphique(g)
 
 
 def test_rotate():
-    assert rotate(45, g) == ruota(45, g)
+    assert rotate(45, g) == pivote(45, g)
 
 
 def test_beside():
-    assert beside(g, g) == accanto(g, g)
+    assert beside(g, g) == cote_a_cote(g, g)
 
 
 def test_above():
-    assert above(g, g) == sopra(g, g)
+    assert above(g, g) == au_dessus(g, g)
 
 
 def test_overlay():
-    assert overlay(g, g) == sovrapponi(g, g)
+    assert overlay(g, g) == superpose(g, g)
 
 
 def test_compose():
-    assert compose(g, g) == componi(g, g)
+    assert compose(g, g) == fr_compose(g, g)
 
 
-def test_fissa():
-    assert pin(top_left, g) == fissa(alto_sinistra, g)
-    assert pin(center, g) == fissa(centro, g)
-    assert pin(bottom_right, g) == fissa(basso_destra, g)
+def test_pin():
+    assert pin(top_left, g) == ancre(haut_gauche, g)
+    assert pin(center, g) == ancre(centre, g)
+    assert pin(bottom_right, g) == ancre(bas_droite, g)
+
+def test_operation_localized_repr():
+    composed = compose(g, g)
+    assert_repr(composed, "fr")
+    assert "compose" in repr(composed)
