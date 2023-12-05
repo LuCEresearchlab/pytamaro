9d85826c159c442e9d7be7c477aaf462f36eba89
Luca Chiodini <luca@chiodini.org>
2023-12-05 17:09:08 +0100

    Release v0.6.1

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = "2021-2023, LuCE Research Lab"
 author = "LuCE Research Lab"
 
 # The full version, including alpha/beta/rc tags
-release = "0.5.2"
+release = "0.6.1"
 
 
 # -- General configuration ---------------------------------------------------
