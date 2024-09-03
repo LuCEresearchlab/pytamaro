b5a92890eea107d00b3e72ffb484bed4dce54491
Davide Frova <frovad@usi.ch>
2024-09-03 10:47:18 +0200

    Fixed missing multi language standardisation in localised primitives tests

--- tests/fr/test_primitives.py
+++ tests/fr/test_primitives.py
@@ -1,38 +1,30 @@
-from pytamaro.color_names import red
-from pytamaro.fr.primitives import ellipse as fr_ellipse
-from pytamaro.fr.primitives import graphique_vide
-from pytamaro.fr.primitives import rectangle as fr_rectangle
-from pytamaro.fr.primitives import secteur_circulaire, texte
-from pytamaro.fr.primitives import triangle as fr_triangle
-from pytamaro.primitives import circular_sector
-from pytamaro.primitives import ellipse as en_ellipse
-from pytamaro.primitives import empty_graphic
-from pytamaro.primitives import rectangle as en_rectangle
-from pytamaro.primitives import text
-from pytamaro.primitives import triangle as en_triangle
+import pytamaro as _pytamaro
+from pytamaro.fr.primitives import (ellipse, rectangle, triangle,
+                                    graphique_vide, secteur_circulaire, texte)
+from pytamaro.fr.color_names import rouge
 from tests.testing_utils import HEIGHT, RADIUS, WIDTH
 
 
 def test_rectangle():
-    assert en_rectangle(WIDTH, HEIGHT, red) == fr_rectangle(WIDTH, HEIGHT, red)
+    assert _pytamaro.rectangle(WIDTH, HEIGHT, _pytamaro.red) == rectangle(WIDTH, HEIGHT, rouge)
 
 
 def test_empty_graphic():
-    assert empty_graphic() == graphique_vide()
+    assert _pytamaro.empty_graphic() == graphique_vide()
 
 
 def test_ellipse():
-    assert en_ellipse(WIDTH, HEIGHT, red) == fr_ellipse(WIDTH, HEIGHT, red)
+    assert _pytamaro.ellipse(WIDTH, HEIGHT, _pytamaro.red) == ellipse(WIDTH, HEIGHT, rouge)
 
 
 def test_text():
-    assert text("hello", "", 12, red) == texte("hello", "", 12, red)
+    assert _pytamaro.text("hello", "", 12, _pytamaro.red) == texte("hello", "", 12, rouge)
 
 
 def test_circular_sector():
-    assert circular_sector(
-        RADIUS, 360, red) == secteur_circulaire(RADIUS, 360, red)
+    assert _pytamaro.circular_sector(
+        RADIUS, 360, _pytamaro.red) == secteur_circulaire(RADIUS, 360, rouge)
 
 
 def test_equilateral_triangle():
-    assert en_triangle(WIDTH, WIDTH, 60, red) == fr_triangle(WIDTH, WIDTH, 60, red)
+    assert _pytamaro.triangle(WIDTH, WIDTH, 60, _pytamaro.red) == triangle(WIDTH, WIDTH, 60, rouge)
