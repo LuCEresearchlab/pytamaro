dd9ef123fbd00fcb7d8028c3098c4e5e547b6e30
Luca Chiodini <luca@chiodini.org>
2024-11-25 16:22:12 +0100

    judicious-docs: add examples and generate output programmatically
    Add a curated set of examples.
    The "transformer" script localizes them using the language-specific API and generates the corresponding output.
    This data extends what Sphinx produces.
    
    (Having the examples inside the docstring would have been better, but it is almost impossible to use RST to encode nice-looking examples for the HTML builder *and* easily extract that data for Judicious.)

--- docs/judicious_transformer.py
+++ docs/judicious_transformer.py
@@ -1,5 +1,8 @@
+import ast
 import json
+import os
 import subprocess
+import sys
 from pathlib import Path
 
 import lxml.etree as ET
@@ -28,17 +31,31 @@ def postprocessor(path, key, value):
 
 def pytamaro_module(folder_name):
     if folder_name == "English":
-        return "pytamaro"
+        return "pytamaro", "en"
     elif folder_name == "French":
-        return "pytamaro.fr"
+        return "pytamaro.fr", "fr"
     elif folder_name == "German":
-        return "pytamaro.de"
+        return "pytamaro.de", "de"
     elif folder_name == "Italian":
-        return "pytamaro.it"
+        return "pytamaro.it", "it"
+
+
+def localize_code(code, lang):
+    sys.path.append(os.path.join(os.path.dirname(__file__), '../pytamaro'))
+    from localization import translations
+    tree = ast.parse(code)
+    # Replace all names in the tree with their localized version
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Name):
+            if node.id in translations:
+                node.id = translations[node.id][lang]
+    code = ast.unparse(tree)
+    return code
 
 
 for languagedir in [el for el in Path("_build/xml").iterdir() if el.is_dir()]:
-    module_name = pytamaro_module(languagedir.name)
+    module_name = pytamaro_module(languagedir.name)[0]
+    print("Processing", languagedir)
     docs = {"module": module_name, "summary": f"PyTamaro graphics ({languagedir.name} API)", "elements": []}
     for filename in languagedir.glob("*.xml"):
         xml = ET.parse(filename)
@@ -47,6 +64,23 @@ for languagedir in [el for el in Path("_build/xml").iterdir() if el.is_dir()]:
         maybe_elements = new_xml_as_dict["elements"]
         new_elements = maybe_elements["element"] if maybe_elements is not None else []
         docs["elements"].extend(new_elements)
+
+    # Execute code examples for PyTamaro (localizing names) and add the output to the docs 
+    with open("examples-pytamaro.json", "r") as f:
+        examples_dict = json.load(f)
+    for element in examples_dict["elements"]:
+        lang_code = pytamaro_module(languagedir.name)[1]
+        element["name"] = localize_code(element["name"], lang_code)
+        if "examples" in element:
+            for example in element["examples"]:
+                example["code"] = localize_code(example["code"], lang_code)
+                output = subprocess.run([sys.executable, "-c", f'from {module_name} import *; {example["code"]}'], 
+                                        capture_output=True, text=True, env={"PYTAMARO_OUTPUT_DATA_URI": "1"}, check=True)
+                example["stdout"] = output.stdout
+            for el in docs["elements"]:
+                if el["name"] == element["name"]:
+                    el["examples"] = element["examples"]
+
     output_path = OUTPUT_DIR / (module_name + ".json")
     with open(output_path, "w") as f:
         f.write(json.dumps(docs, indent=4))
